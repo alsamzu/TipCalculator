f372a7a431a9bffeb7e725fca103c25106a9a44e
alsamzu <zulfalsam@gmail.com>
2018-11-26 20:52:24 +0300

    Fun facts App

--- app/src/main/java/com/example/zulfa/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/zulfa/tipcalculator/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.zulfa.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+        private double billAmount = 0.0;
+        private double percent = 0.15;
+        private TextView amountTextView;
+        private  TextView percentTextView;
+        private TextView tipTextView;
+        private TextView totalTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView) findViewById(R.id.amountEditText);
     }
 }
